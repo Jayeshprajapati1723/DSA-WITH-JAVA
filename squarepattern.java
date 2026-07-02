91c114ec08f23b41a18c2679310c225902337342
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-02 16:26:08 +0530

    git ignore  file

---
+++ squarepattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class squarepattern {
+    public static void main(String[] args) {
+        System.out.println("Enter a number to get square pattern eg 5");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // square pattern
+        for (int i = 1; i <= n; i++) {
+            // row
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
+/*output if n = 10
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+*/
