faf74a759abb7b9da73957ce07a3c6dec4f86945
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-02 20:27:17 +0530

    square hollow  pattern solve

---
+++ hollowsquare.java
@@ -0,0 +1,29 @@
+public class hollowsquare {
+    public static void main(String[] args) {
+        // hollow rectangle
+        int n = 15;
+        for (int j = 1; j <= n; j++) {
+            System.out.print("*");
+        }
+        for (int s = 1; s <= n - 1; s++) {
+            System.out.println();
+            System.out.print("*");
+            for (int t = 2; t <= n - 1; t++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+
+        }
+
+        System.out.println();
+        for (int j = 1; j <= n; j++) {
+            System.out.print("*");
+        }
+
+    }
+}
+// *****
+// * *
+// * *
+// * *
+// *****
