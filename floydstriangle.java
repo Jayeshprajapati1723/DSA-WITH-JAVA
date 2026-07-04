52db1bd4896782f2cef6a5c039bd526bcf07aadc
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-04 23:19:51 +0530

    floyds triangle

---
+++ floydstriangle.java
@@ -0,0 +1,16 @@
+public class floydstriangle {
+    public static void main(String[] args) {
+        int n = 5 ;
+  System.out.print(1) ;
+  System.out.println();
+for(int i=1; i<=n; i++) {
+for(int j=i+1;j<=i+j;j++){
+System.out.print(j) ;
+}
+System.out.println() ;
+}
+}
+}
+// 1
+// 2 3
+// 3 4 5
