5cffb6166efb5e0af77cab60887c4616d40b7d5b
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-05 17:07:22 +0530

    floyds triangle standard or my logic

--- floydstriangle.java
+++ floydstriangle.java
@@ -1,16 +1,51 @@
+// floads triangle with my logic not standard 
 public class floydstriangle {
     public static void main(String[] args) {
-        int n = 5 ;
-  System.out.print(1) ;
-  System.out.println();
-for(int i=1; i<=n; i++) {
-for(int j=i+1;j<=i+j;j++){
-System.out.print(j) ;
+
+        int n = 100;
+        if (n <= 0) {
+            System.out.print("enter natural no");
+        } else {
+            int s = 1;
+            int e = 3;
+            System.out.print(1);
+            System.out.println();
+            System.out.print(2 + " " + 3);
+            System.out.println();
+            for (int i = 3; i <= n; i++) {
+                for (int j = i + s; j <= i + e; j++) {
+                    System.out.print(j + " ");
+                }
+                s = e;
+                e = e + i;
+                System.out.println();
+            }
+        }
+        System.out.println("standard floyds counter method ") ;
+        int ct = 1 ;
+    for(int i=1 ; i<=n ;i++) {
+        for(int j=1 ;j<=i ;j++) {
+System.out.print(ct+" ") ;
+     ct++ ;
+        }
+   System.out.println() ;
+    }
+    }
 }
-System.out.println() ;
-}
-}
-}
-// 1
-// 2 3
-// 3 4 5
+/*
+ * 1
+ * 2 3
+ * 4 5 6
+ * 7 8 9 10
+ * 11 12 13 14 15
+ * 16 17 18 19 20 21
+ * 22 23 24 25 26 27 28
+ * 29 30 31 32 33 34 35 36
+ * 37 38 39 40 41 42 43 44 45
+ * 46 47 48 49 50 51 52 53 54 55
+ * 56 57 58 59 60 61 62 63 64 65 66
+ * 67 68 69 70 71 72 73 74 75 76 77 78
+ * 79 80 81 82 83 84 85 86 87 88 89 90 91
+ * 92 93 94 95 96 97 98 99 100 101 102 103 104 105
+ * 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120
+ */
