7419a16582a82356d9d827fe24cd3f45b99d8de8
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-17 23:30:20 +0530

    center diamond  pattern

--- diomond.java
+++ diomond.java
@@ -12,5 +12,31 @@ System.out.print("*") ;
 System.out.println(); 
 ct++;
 } 
+
+    for(int i=n ;i>=1 ;i--)
+ {
+
+    for(int j=1 ;j <= 2*i-1 ;j++) {
+System.out.print("*") ;
+    }
+
+System.out.println(); 
+        for(int s =i ;s<=n ;s++) {
+        System.out.print(" ") ;
+    }
+} 
+
 }   
 }
+/*
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+*/
