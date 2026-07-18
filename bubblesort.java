56c130910f4289b0494bc4c252fbea7c478fbdc2
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-18 23:30:15 +0530

    bubble sort

--- bubblesort.java
+++ bubblesort.java
@@ -1,16 +1,26 @@
 public class bubblesort {
     public static void main(String[] args) {
-        int arr[]={1,3,7,8,2} ;
-        System.out.print(arr.length) ;
-for(int i=0;i<arr.length-1 ;i++) {
-    if(arr[i]>arr[i+1] ) {
-        int temp = arr[i] ;
-        arr[i] = arr[i+1] ;
-        arr[i+1] =temp ;
+        int arr[]={1,3,7,8,2,0} ;
+   
+        System.out.println(arr.length) ;
+   for(int i=0;i<arr.length ;i++){
+        System.out.print(arr[i] +" ") ;
+    }     
+for(int i=0;i<arr.length-1 ;i++){
+
+        for(int j=0;j<arr.length-1 ;j++) {
+    if(arr[j]>arr[j+1] ) {
+        int temp = arr[j] ;
+        arr[j] = arr[j+1] ;
+        arr[j+1] =temp ;
     }
 }
+
+}
+
+System.out.println("array is sorted by bubble sorting ") ;
 for(int i=0 ;i<arr.length;i++){
-    System.out.println(arr[i]) ;
+    System.out.print(arr[i] +" ") ;
 }
 
     }
