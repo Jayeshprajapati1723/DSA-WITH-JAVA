87a35cc3b88c01b4bc0ecd6ab78b559988a89709
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-21 23:56:06 +0530

    binary search

--- binarysrch.java
+++ binarysrch.java
@@ -1,24 +1,28 @@
 
 public class binarysrch {
     public static void main(String[] args) {
-        int target = 12;
+        int target = 12500;
         int mid = 0;
-        int arr[] = { 2, 4, 6, 8, 12, 14 };
-        int i = 1;
-        int j = arr.length; int ct = arr.length ;
+        int arr[] = { 2, 4, 6, 8, 12, 14, 25,1250 };
+        int i = 0;
+        int ct =arr.length-1 ;
+        int j = arr.length - 1;
         while (ct !=0) {
-            mid = i + j / 2;
+            mid = (i + j) / 2;
             if (arr[mid] == target) {
-                System.out.println("founded target =" + arr[mid] + "on index no" + mid);
+        ct = 1 ;
             } else if (arr[mid] > target) {
-                mid = mid - 1;
-            } else {
-                mid = mid + 1;
-                
-            }
-       ct -- ;
+                j = mid - 1;
+            } else if (arr[mid] < target) {
+                i = mid + 1;
+            }      
+            ct-- ;
         }
-
-
+if(target == arr[mid]) {
+        System.out.println("founded target =" + arr[mid] + "on index no" + mid);
+} 
+            else {
+                System.out.println("not founded in the array " +target) ;
+            }
     }
 }
