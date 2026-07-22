6eaf50a87b77f79c6f854d1d242ccca84f702044
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-22 23:22:53 +0530

    insertion sort

--- insertion.java
+++ insertion.java
@@ -1,10 +1,10 @@
 public class insertion {
     public static void main (String args[]) {
-int usarr[] ={5,3,4,1,2} ;int n= usarr.length;
-int sortarr[]={} ;
+int usarr[] ={5,3,4,1,2} ;
+int sortarr[]={0,0,0,0} ;
 sortarr[0] = usarr[0] ;
  
-for(int i=1 ; i<usarr.length ;i++) {
+for(int i=1 ; i<usarr.length-1 ;i++) {
 if(sortarr[i-1]>usarr[i])
 {
     sortarr[i] = sortarr[i-1] ;
