6383c0ca03d7b2208e82cfa417f602535e99264d
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-07-28 22:48:50 +0530

    finaly completed

--- InsertionSortDemo.java
+++ InsertionSortDemo.java
@@ -5,7 +5,8 @@ public class InsertionSortDemo {
         // Outer loop: Unsorted elements ko ek-ek karke uthane ke liye (Index 1 se start)
         for (int i = 1; i < arr.length; i++) {
             int key = arr[i]; // Jise sahi jagah fit karna hai (e.g. key = 3)
-            int j = i - 1;    // Left side ke sorted elements ka last index
+            int j = i - 1;  
+              // Left side ke sorted elements ka last index
 
             // Inner loop: Jab tak peeche ke elements 'key' se bade hain, unhe aage SHIFT karo
             while (j >= 0 && arr[j] > key) {
